17618c044ab5ff42c5b6369d4ddc1aa47dc854ce
LM&T <tbbragg83@gmail.com>
2020-07-25 23:26:11 -0500

    still working on it

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,32 @@ var dbData = require("../db/db.json");
 
 module.exports = function(app) {
 
-    app.get("/api/notes", function(req, res) {
-        res.json(fs.readFile(dbData, "utf8"));
-      });
-      
-    app.post("/api/notes", function(req, res) {
+  app.get("/api/notes", function(req, res) {
+    fs.readFile(dbData, "utf8", function(err, data) {
+      if (err) {
+        throw err;
+    }
+      var parsedNote = JSON.parse(data);
+      res.json(parsedNote);
+    })
 
-        console.log("Working!");
-      });
-}
+  });
+      
+  app.post("/api/notes", function(req, res) {
+    fs.readFile(dbData, "utf8", function(err, data) {
+      if (err) { 
+        throw err;
+      }
+      var parsedNote = JSON.parse(data);
+      var newNote = req.body
+      var ID = (notes.length).toString();
+      newNote.id = ID
+      parsedNote.push(newNote);
+      fs.writeFileSync(dbData, JSON.stringify(parsedNote));
+      res.json(parsedNote);
+    });
+
+  });
+
+};
+
