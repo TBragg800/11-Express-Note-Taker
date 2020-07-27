235bbb854ba3d8d902a935f0ea768c2afb821636
LM&T <tbbragg83@gmail.com>
2020-07-27 04:40:54 -0500

    working application

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,38 @@
+
 const fs = require("fs");
-var dbData = require("../db/db.json");
+var dbData = "./db/db.json";
+var parsedNotes = JSON.parse(fs.readFileSync(dbData, 'utf8'));
 
 module.exports = function(app) {
-
-  app.get("/api/notes", function(req, res) {
-    fs.readFile(dbData, "utf8", function(err, data) {
-      if (err) {
-        throw err;
-    }
-      var parsedNote = JSON.parse(data);
-      res.json(parsedNote);
-    })
-
-  });
-      
-  app.post("/api/notes", function(req, res) {
-    fs.readFile(dbData, "utf8", function(err, data) {
-      if (err) { 
-        throw err;
-      }
-      var parsedNote = JSON.parse(data);
-      var newNote = req.body
-      var ID = (notes.length).toString();
-      newNote.id = ID
-      parsedNote.push(newNote);
-      fs.writeFileSync(dbData, JSON.stringify(parsedNote));
-      res.json(parsedNote);
+    app.get("/api/notes", (req, res) => {
+        res.json(parsedNotes);
     });
 
-  });
+    app.get("/api/notes/:id", (req, res) => {
+        res.json(parsedNotes[(req.params.id)]);
+    });
 
+    app.post("/api/notes", (req, res) => {
+        var newNote = req.body;
+        newNote.id = (parsedNotes.length) + 1;
+        parsedNotes.push(newNote);
+        fs.writeFileSync(dbData, JSON.stringify(parsedNotes));
+        res.json(parsedNotes)
+    });
+
+    app.delete("/api/notes/:id", (req, res) => {
+        let noteId = req.params.id;
+        parsedNotes = parsedNotes.filter(currentValue => {
+            return currentValue.id != noteId;
+        });
+        let noteId2 = 1;
+        for (currentValue of parsedNotes) {
+            currentValue.id = noteId2;
+            noteId2++;
+        }
+
+        fs.writeFileSync(dbData, JSON.stringify(parsedNotes));
+        res.json(parsedNotes);
+    });
 };
-
 
